6eae81f01ee3fca32ca9f395a534f31904dd713a
annisadiann <annisadian0412@gmail.com>
2024-05-10 09:29:46 +0700

    Add project files.

---
+++ stackusing/stackusing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// Node class representing a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
